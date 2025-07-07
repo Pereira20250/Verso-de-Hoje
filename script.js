e9b2e45ea675529365bc939e5dc4b8449c690cfb
Dev2025 <matheus.pereira@aluno.liceuja.com>
2025-07-07 11:57:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,19 @@ const versiculos = [
   { texto: 'Entrega o teu caminho ao Senhor; confia nele, e ele tudo fará.', referencia: 'Salmo 37:5' },
   { texto: 'Mil cairão ao teu lado e dez mil à tua direita, mas tu não serás atingido', referencia: 'Salmo 91:7'},
   { texto: 'Entregue o teu caminho ao Senhor; confia nele, e ele tudo fará.', referencia: 'Salmo 37:5'},
-  { texto: 'Não temas pois eu sou contigo, e não te assombres pois eu sou o teu Deus.', referencia: ''Isaias '41:10'},
+  { texto: 'Não temas pois eu sou contigo, e não te assombres pois eu sou o teu Deus.', referencia: 'Isaias 41:10'},
+  { texto: 'Não fui eu que ordenei a você? Seja forte e corajoso! Não se apavore nem desanime, pois o Senhor, o seu Deus, estará com você por onde você andar".', referencia: 'Josué 1:9'},
+  { texto: 'Consagre ao Senhor tudo o que você faz, e os seus planos serão bem-sucedidos.', referencia: 'Provérbios 16:3'},
+  { texto: 'Porque sou eu que conheço os planos que tenho para vocês', 'diz o Senhor', 'planos de fazê-los prosperar e não de causar dano, planos de dar a vocês esperança e um futuro.', referencia: 'Jeremias 29:11'},
+  { texto: '"Venham a mim, todos os que estão cansados e sobrecarregados, e eu darei descanso a vocês.', referencia: 'Mateus 11:28'},
+  { texto: 'Lancem sobre ele toda a sua ansiedade, porque ele tem cuidado de vocês.', referencia: 'Mateus 6:33'},
+  { texto: 'Não andem ansiosos por coisa alguma, mas em tudo, pela oração e súplicas, e com ação de graças, apresentem seus pedidos a Deus.', referencia: 'Filipenses 4:6'},
+  { texto: 'Assim, permanecem agora estes três: a fé, a esperança e o amor. O maior deles, porém, é o amor.', referencia: '1 Coríntios 13:13'},
+  { texto: 'Acima de tudo, porém, revistam-se do amor, que é o elo perfeito.', referencia: 'Colossenses 3:14'},
+  { texto: 'Deus converte o deserto em lago e a terra seca em fontes.', referencia: 'Salmos 107:35'},
+  { texto: 'Busquem, pois, em primeiro lugar o Reino de Deus e a sua justiça, e todas essas coisas lhes serão acrescentadas.', referencia: 'Mateus 6:33'},
+  { texto: 'Escondi a tua palavra no meu coração, para eu não pecar contra ti.', referencia: 'Salmos 119:11'},
+  { texto: 'A esperança adiada desfalece o coração, mas o desejo atendido é árvore', referencia: 'Provérbios 13:12'},
 ];
 
 function getTodayKey() {
